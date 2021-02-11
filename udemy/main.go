abeab38338562450acb8be5977089f49c2dcd5f5
rulisastra <ruli.sastra.p@mail.ugm.ac.id>
2021-02-11 16:36:36 +0700

    update udemy

---
+++ udemy/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func mulai(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Selamat! karena sudah memulai hal-hal kecil!")
+}
+
+func main() {
+
+	// define routes, post, put, dll
+	http.HandleFunc("/mulai", mulai)
+
+	// starting server
+	log.Fatal(http.ListenAndServe("localhost:8082", nil))
+
+	// liat di log
+	fmt.Print("Starting server at http://localhost:8082")
+}
